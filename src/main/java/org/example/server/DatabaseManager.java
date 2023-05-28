7f216e89e59325e123181954fe5a6f3bf0286fa2
LRistiana <liangga4@gmail.com>
2023-05-28 18:14:29 +0800

    membuat class DatabaseManager untuk connection db

---
+++ src/main/java/org/example/server/DatabaseManager.java
@@ -0,0 +1,29 @@
+package org.example.server;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseManager {
+    private Connection connection;
+
+    public DatabaseManager(String databaseUrl) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + databaseUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
