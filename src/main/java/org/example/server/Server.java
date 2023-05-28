5c6169a16b3fb3ca2c2728e6c27250f45ee8fe1a
LRistiana <liangga4@gmail.com>
2023-05-28 18:07:20 +0800

    membuat GET user dan GET user berdasarkan id

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,29 +1,79 @@
 package org.example.server;
 
-import java.io.IOException;
+import java.io.*;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.example.controller.ProductsController;
+import org.example.controller.UsersController;
+
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server{
-    public static void main(String[]args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost",8067), 0);
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-        httpServer.createContext("/",null);
+
+public class Server{
+    private static final int PORT = 8067;
+    private UsersController usersController;
+    private ProductsController productsController;
+
+    public Server(){
+        DatabaseManager databaseManager = new DatabaseManager("database/db_makanan_online.db");
+        usersController = new UsersController(databaseManager);
+        productsController = new ProductsController(databaseManager);
+    }
+    public void httpConnection() throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost",PORT), 0);
+        httpServer.createContext("/",new Handler());
         httpServer.setExecutor(Executors.newSingleThreadExecutor());
         httpServer.start();
-
     }
-    public class Handler implements HttpHandler{
+    private class Handler implements HttpHandler{
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            if ("GET".equals(exchange.getRequestMethod())){
+            String method = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            String response = "";
+            OutputStream outputStream = exchange.getResponseBody();
+            if (path.equals("/users") && method.equals("GET")){
+                JSONArray jsonArray = usersController.getUser();
+                response = jsonArray.toString();
+                exchange.sendResponseHeaders(200, response.length());
+                outputStream.write(response.getBytes());
+            }
+            else if (path.matches("/users/\\d+") && method.equals("GET")){
+                int userId = Integer.parseInt(path.substring(path.lastIndexOf('/')+1));
+                JSONArray jsonArray = usersController.getUser(userId);
+                if ( jsonArray != null){
+                    response = jsonArray.toString();
+                    exchange.sendResponseHeaders(200, response.length());
+
+                }else {
+                    response = " User Not Found";
+                    exchange.sendResponseHeaders(404, response.length());
+
+                }
+                outputStream.write(response.getBytes());
+            }
+            else if (path.equals("/users") && method.equals("DELETE")) {
 
             }
+            else if(path.equals("/products") && method.equals("GET")){
+
+            }
+
+            outputStream.flush();
+            outputStream.close();
         }
+//        private JSONObject parseRequestBody(InputStream requestBody) throws IOException {
+//            byte[] requestBodyBytes = requestBody.readAllBytes();
+//            String requestBodyString = new String(requestBodyBytes);
+//            return new JSONObject(requestBodyString);
+//        }
     }
 }
