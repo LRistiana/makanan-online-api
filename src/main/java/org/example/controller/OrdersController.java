d55860b45289c6958fdaa4f315c76a2b0b21637e
LRistiana <liangga4@gmail.com>
2023-05-30 12:39:48 +0800

    update getOrder by id agar menampilkan lebih banyak informasi

--- src/main/java/org/example/controller/OrdersController.java
+++ src/main/java/org/example/controller/OrdersController.java
@@ -39,20 +39,63 @@ public class OrdersController {
     }
     public JSONArray getOrders(int idOrder){
         JSONArray jsonArray = new JSONArray();
-        String querySql = "SELECT * FROM orders WHERE id=" + idOrder;
+        String querySql = "SELECT * FROM orders " +
+                "INNER JOIN users " +
+                "ON orders.buyer=users.id WHERE orders.id=" + idOrder;
+        String querySqlDetail = "SELECT * FROM orders_details " +
+                "INNER JOIN products " +
+                "ON products.id=orders_details.'order' " +
+                "WHERE orders_details.'order'=" + idOrder;
+        String querySqlReview = "SELECT * FROM reviews WHERE reviews.'order'="+idOrder;
 
         try(Connection connection = databaseManager.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(querySql)){
             while (resultSet.next()){
-                JSONObject jsonUser = new JSONObject();
-                jsonUser.put("id",resultSet.getInt("id"));
-                jsonUser.put("buyer",resultSet.getInt("buyer"));
-                jsonUser.put("note",resultSet.getInt("note"));
-                jsonUser.put("total",resultSet.getInt("total"));
-                jsonUser.put("discount",resultSet.getInt("discount"));
-                jsonUser.put("isPaid",resultSet.getInt("is_paid"));
-                jsonArray.put(jsonUser);
+                JSONObject jsonOrder = new JSONObject();
+                jsonOrder.put("id",resultSet.getInt("id"));
+                jsonOrder.put("buyer",resultSet.getString("first_name") + " " + resultSet.getString("last_name"));
+                jsonOrder.put("email",resultSet.getString("email"));
+                jsonOrder.put("phoneNumber",resultSet.getString("phone_number"));
+                jsonOrder.put("note",resultSet.getInt("note"));
+                jsonOrder.put("total",resultSet.getInt("total"));
+                jsonOrder.put("discount",resultSet.getInt("discount"));
+                jsonOrder.put("isPaid",resultSet.getInt("is_paid"));
+
+                JSONArray jsonDetailArray = new JSONArray();
+                try(
+                    Statement statementDetail = connection.createStatement();
+                    ResultSet resultSetDetail = statementDetail.executeQuery(querySqlDetail)){
+                    while (resultSetDetail.next()){
+                        JSONObject jsonOrderDetail = new JSONObject();
+                        jsonOrderDetail.put("product",resultSetDetail.getString("title"));
+                        jsonOrderDetail.put("quantity",resultSetDetail.getInt("quantity"));
+                        jsonOrderDetail.put("price",resultSetDetail.getInt("price"));
+                        jsonDetailArray.put(jsonOrderDetail);
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                jsonOrder.put("Order_detail", jsonDetailArray);
+
+                JSONArray jsonReviews = new JSONArray();
+                try(
+                        Statement statementReview = connection.createStatement();
+                        ResultSet resultSetReview = statementReview.executeQuery(querySqlReview)){
+                    while (resultSetReview.next()){
+                        JSONObject jsonReview = new JSONObject();
+                        jsonReview.put("star",resultSetReview.getInt("star"));
+                        jsonReview.put("description",resultSetReview.getString("description"));
+                        jsonReviews.put(jsonReview);
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                jsonOrder.put("reviews", jsonReviews);
+
+
+                //================END OF JSONARRAY==============//
+                jsonArray.put(jsonOrder);
             }
         }catch (SQLException e){
             e.printStackTrace();
